57c0710016d5101b68981d40855f0fba2cb597ec
yangha Kwon <ha996800@gmail.com>
2023-05-15 22:13:56 +0900

    first commit

---
+++ app/src/main/java/com/example/registerlogin/Searching.java
@@ -0,0 +1,116 @@
+package com.example.registerlogin;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Searching extends AppCompatActivity {
+
+    private List<String> list;          // 데이터를 넣은 리스트변수
+    private ListView listView;          // 검색을 보여줄 리스트변수
+    private EditText editSearch;        // 검색어를 입력할 Input 창
+    private SearchingAdapter adapter;      // 리스트뷰에 연결할 어답터
+    private ArrayList<String> arraylist;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searching);
+
+        editSearch = (EditText) findViewById(R.id.editSearch);
+        listView = (ListView) findViewById(R.id.Searchlistview);
+        // 커서를 조금 띄운다.
+        editSearch.setPadding(40, 40, 40, 40);
+
+        // 리스트를 생성한다.
+        list = new ArrayList<String>();
+
+        // 검색에 사용할 데이터을 미리 저장한다.
+        settingList();
+
+        // 리스트의 모든 데이터를 arraylist에 복사한다.
+        arraylist = new ArrayList<String>();
+        // list 복사본을 만든다.
+        arraylist.addAll(list);
+
+        // 리스트에 연동될 아답터를 생성한다.
+        adapter = new SearchingAdapter(list, this);
+
+        // 리스트뷰에 아답터를 연결한다.
+        listView.setAdapter(adapter);
+
+        // input창에 검색어를 입력시 "addTextChangedListener" 이벤트 리스너를 정의한다.
+        editSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // input창에 문자를 입력할때마다 호출된다.
+                // search 메소드를 호출한다.
+                String text = editSearch.getText().toString();
+                search(text);
+            }
+        });
+
+
+    }
+
+    // 검색을 수행하는 메소드
+    public void search(String charText) {
+
+        // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
+        list.clear();
+
+        // 문자 입력이 없을때는 모든 데이터를 보여준다.
+        if (charText.length() == 0) {
+            list.addAll(arraylist);
+        }
+        // 문자 입력을 할때..
+        else
+        {
+            // 리스트의 모든 데이터를 검색한다.
+            for(int i = 0;i < arraylist.size(); i++)
+            {
+                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
+                if (arraylist.get(i).toLowerCase().contains(charText))
+                {
+                    // 검색된 데이터를 리스트에 추가한다.
+                    list.add(arraylist.get(i));
+                }
+            }
+        }
+        // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
+        adapter.notifyDataSetChanged();
+    }
+
+    // 검색에 사용될 데이터를 리스트에 추가한다.
+    private void settingList(){
+        list.add("유리");
+        list.add("플라스틱");
+        list.add("재활용");
+        list.add("페트병");
+        list.add("두꺼운 종이");
+        list.add("일반 쓰레기");
+        list.add("고무");
+        list.add("가위");
+        list.add("볼펜");
+        list.add("안마기");
+        list.add("스티로폼");
+        list.add("마우스");
+        list.add("주사위");
+        list.add("쓰레기");
+    }
+}
